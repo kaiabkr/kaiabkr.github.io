e462beb757a489c29278a5b125d2e0425f6f2306
kaiabkr <keb9830@nyu.edu>
2023-04-06 00:50:12 -0400

    adding Q2

---
+++ javaQ2/script.js
@@ -0,0 +1,9 @@
+// QUESTION #2
+let num = 14;
+ if (num == 15){
+  alert('You are correct! These two numbers are equal!');
+ } else if (num > 15){
+  alert('This number is greater than 15.');
+  } else{
+  alert('Try Again.');
+ }
