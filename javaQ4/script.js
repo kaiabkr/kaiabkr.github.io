5bd482572a49c26c0d67d21a0eda3816b23e5135
kaiabkr <keb9830@nyu.edu>
2023-04-06 00:51:43 -0400

    adding Q4

---
+++ javaQ4/script.js
@@ -0,0 +1,20 @@
+//QUESTION 4
+ function rainbow(num){
+   if (num == 1){
+     return "Flowers";
+   }
+   else if (num == 2){
+     return "Plants";
+   }
+   else if (num == 3){
+     return "Garden";
+   }
+   else {
+       return "There are only 3 options!";
+   }
+ }
+
+ console.log(door(1));
+ console.log(door(2));
+ console.log(door(3));
+ console.log(door(5));
