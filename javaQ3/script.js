66df8087ce9f80e4cfb3459635768962639d3796
kaiabkr <keb9830@nyu.edu>
2023-04-06 00:50:42 -0400

    adding Q3

---
+++ javaQ3/script.js
@@ -0,0 +1,6 @@
+//QUESTION 3
+function addNumberAndReturn(numberOne, numberTwo) {
+    console.log ("returning the function...") 
+    return numberOne + numberTwo;
+ }
+ console.log(addNumberAndReturn(400,200));
